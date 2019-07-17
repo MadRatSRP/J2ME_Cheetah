9ba8b61dc80e234f18a9ccdd0bfbb406668b3fbe
MadRat <mischa.alpeew@yandex.ru>
2019-07-17 15:00:48 +0300

    Removed apache common io dependency, updated algorith of working with NL_Cheats data, completed working with NL_Data. View elements next.

--- app/src/main/java/com/madrat/j2me_cheetah/MainActivity.kt
+++ app/src/main/java/com/madrat/j2me_cheetah/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.madrat.j2me_cheetah
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -13,18 +14,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val array: List<Cheat> = openFileAndConvertItToTheListOfCheats("nl_cheats_#1.json")
-        Log.d("", "${array.size}")
+        returnListOfCheats(applicationContext)
     }
-    fun openFileAndConvertItToTheListOfCheats(filePath: String): List<Cheat> {
+    fun openFileAndConvertItToTheListOfCheats(context: Context, pathToNLCheats: String,
+                                              fileId: Int): List<Cheat> {
+        val cheatName = getString(R.string.pathToNLCheatsCheat, fileId)
+
         val moshi = Moshi.Builder().build()
         val cheatsParser = CheatsParser(moshi)
 
-        val assetManager = applicationContext.assets//.openFd("PreparedFilesWithCheats/NL_Cheats/")
-        val inputStream = assetManager.open("PreparedFilesWithCheats/NL_Cheats/$filePath")
+        val assetManager = context.assets
+        val inputStream = assetManager.open(pathToNLCheats + cheatName)
 
         val bufferedSource = Okio.buffer(Okio.source(inputStream))
         val listOfCheats = cheatsParser.parse(JsonReader.of(bufferedSource))
+        return listOfCheats
+    }
+    fun returnListOfCheats(context: Context): ArrayList<Cheat> {
+        val listOfCheats = ArrayList<Cheat>()
+
+        val pathToFolderWithPreparedData = getString(R.string.pathToFolderWithPreparedData)
+        val pathToFolderWithNLCheats = getString(R.string.pathToFolderWithNLCheats)
+        val pathToNLCheats = pathToFolderWithPreparedData + pathToFolderWithNLCheats
+
+        val firstListOfCheats
+                = openFileAndConvertItToTheListOfCheats(context, pathToNLCheats, 1)
+        val secondListOfCheats
+                = openFileAndConvertItToTheListOfCheats(context, pathToNLCheats, 2)
+        val thirdListOfCheats
+                = openFileAndConvertItToTheListOfCheats(context, pathToNLCheats, 3)
+        val fourthListOfCheats
+                = openFileAndConvertItToTheListOfCheats(context, pathToNLCheats, 4)
+        val fifthListOfCheats
+                = openFileAndConvertItToTheListOfCheats(context, pathToNLCheats, 5)
+        val sixthListOfCheats
+                = openFileAndConvertItToTheListOfCheats(context, pathToNLCheats, 6)
+
+        listOfCheats.addAll(firstListOfCheats)
+        listOfCheats.addAll(secondListOfCheats)
+        listOfCheats.addAll(thirdListOfCheats)
+        listOfCheats.addAll(fourthListOfCheats)
+        listOfCheats.addAll(fifthListOfCheats)
+        listOfCheats.addAll(sixthListOfCheats)
+
+        Log.d("", "${listOfCheats.size}")
         return listOfCheats
     }
 }
