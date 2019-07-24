f197480351ed15b53676333e3e80fd9aeae9db1c
MadRat <mischa.alpeew@yandex.ru>
2019-07-25 01:00:35 +0300

    Removed autofocus from soc layout, added inner fragments container to activity

--- app/src/main/java/com/madrat/j2me_cheetah/fragment/BrowseCheatView.kt
+++ app/src/main/java/com/madrat/j2me_cheetah/fragment/BrowseCheatView.kt
@@ -7,11 +7,14 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.madrat.j2me_cheetah.R
+import com.madrat.j2me_cheetah.util.addFragment
+import com.madrat.j2me_cheetah.util.removeFragment
 import kotlinx.android.synthetic.main.view_browse_cheat.view.*
 
 class BrowseCheatView: Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        //(activity as AppCompatActivity).addFragment(BrowseCheatButtons.instance, R.id.additionalElementContainer)
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -21,5 +24,10 @@ class BrowseCheatView: Fragment() {
         view.bcTitle.text = cheatArguments?.title
         view.bcDescription.text = cheatArguments?.description
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        //(activity as AppCompatActivity).removeFragment(BrowseCheatButtons.instance)
     }
 }
--- app/src/main/java/com/madrat/j2me_cheetah/util/Extensions.kt
+++ app/src/main/java/com/madrat/j2me_cheetah/util/Extensions.kt
@@ -3,6 +3,8 @@ package com.madrat.j2me_cheetah.util
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.moshi.JsonReader
@@ -23,4 +25,15 @@ fun ViewGroup.inflate(layoutRes: Int): View {
 //RecyclerView extensions
 fun RecyclerView.linearManager() {
     this.layoutManager = LinearLayoutManager(context)
+}
+//Fragment extensions
+fun AppCompatActivity.addFragment(fragment: Fragment, id: Int) {
+    this.supportFragmentManager?.beginTransaction()
+        ?.replace(id, fragment)
+        ?.commit()
+}
+fun AppCompatActivity.removeFragment(fragment: Fragment) {
+    this.supportFragmentManager?.beginTransaction()
+        ?.remove(fragment)
+        ?.commit()
 }
